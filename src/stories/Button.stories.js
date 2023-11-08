53b2c5d90fe8677701e4eb44ab2696c6c99fda4f
Rahaf Mansour <rahafmansour2018@gmail.com>
2023-11-08 18:15:52 +0200

    Add the children arg for the remaining stories

--- src/stories/Button.stories.js
+++ src/stories/Button.stories.js
@@ -23,35 +23,41 @@ Secondary.args = {
 export const Text = Template.bind({});
 Text.args = {
   variant: "text",
-  children: "Text Button",
+  children: "Text",
 };
 
 export const Small = Template.bind({});
 Small.args = {
   size: "small",
+  children: "Small Button",
 };
 
 export const Medium = Template.bind({});
 Medium.args = {
   size: "medium",
+  children: "Medium Button",
 };
 
 export const Large = Template.bind({});
 Large.args = {
   size: "large",
+  children: "Large Button",
 };
 
 export const Red = Template.bind({});
 Red.args = {
   color: "red",
+  children: "Red Button",
 };
 
 export const Blue = Template.bind({});
 Blue.args = {
   color: "blue",
+  children: "Blue Button",
 };
 
 export const Green = Template.bind({});
 Green.args = {
   color: "green",
+  children: "Green Button",
 };
